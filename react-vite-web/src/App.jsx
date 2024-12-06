2689596dac7134b648f322dd2ebe5f5a73ec3f0f
Maksim <a2vend0kt@gmail.com>
2024-12-06 02:16:08 +0200

    Header footer
    Need do a header and in footer need do a icons and links , and start do body

---
+++ react-vite-web/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import Footer from "./components/footer" // Ensure this matches the actual file name
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import './components/Header.css';
+import './components/Footer.css';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+      
+    </>
+  );
+}
+
+export default App;
